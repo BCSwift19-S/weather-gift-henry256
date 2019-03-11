1e38a14aaefa732588ef5d2b128af8411ecbb3b0
Henry Chen <46900345+henry256@users.noreply.github.com>
2019-03-11 14:46:34 -0400

    Add files via upload

---
+++ WeatherGift/WeatherGift/DetailVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by user150412 on 3/11/19.
+//  Copyright © 2019 user150412. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationLabel.text = locationsArray[currentPage]
+       
+    }
+    
+
+   
+
+}
---
+++ WeatherGift/WeatherGift/ListVC.swift
@@ -0,0 +1,21 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by user150412 on 3/11/19.
+//  Copyright © 2019 user150412. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
