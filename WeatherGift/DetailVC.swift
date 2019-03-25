412fcdb7cf5fb68b0cb7c9a95bb7fed60c90767d
Henry Chen <46900345+henry256@users.noreply.github.com>
2019-03-25 01:46:00 -0400

    Add files via upload
    Issue with commiting pod file. telling me its too large

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [WeatherLocation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationLabel.text = locationsArray[currentPage].name
+        dateLabel.text = locationsArray[currentPage].coordinates
+        
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by user150412 on 3/25/19.
+//  Copyright © 2019 user150412. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name = ""
+    var coordinates = ""
+}
