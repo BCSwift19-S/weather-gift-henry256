6bfd837c1e534a982f96dd26d25f82e1a3085069
Henry Chen <46900345+henry256@users.noreply.github.com>
2019-04-01 21:59:48 -0400

    Add files via upload

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -7,92 +7,24 @@
 //
 
 import UIKit
-import CoreLocation
 
 class DetailVC: UIViewController {
 
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var summaryLabel: UILabel!
-    
-    @IBOutlet weak var imageLabel: UIImageView!
-    
-    var currentPage = 0
-    var locationsArray = [WeatherLocation]()
-    var locationManager: CLLocationManager!
-    var currentLocation: CLLocation!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUserInterface()
-        
-       
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        if currentPage == 0{
-            getLocation()
-        }
-    }
-    func updateUserInterface(){
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
-        
-    }
 
-}
-extension DetailVC: CLLocationManagerDelegate{
+        // Do any additional setup after loading the view.
+    }
     
-    func getLocation(){
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        let status = CLLocationManager.authorizationStatus()
-        handleLocationAuthorizeStatus(status: status)
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    func handleLocationAuthorizeStatus(status: CLAuthorizationStatus){
-        switch status {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .authorizedAlways, .authorizedWhenInUse:
-            locationManager.requestLocation()
-        case .denied:
-            print("can't show location. User has not authorized its use")
-        case .restricted:
-            print("Access denied")
-        }
-    }
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        handleLocationAuthorizeStatus(status: status)
-    }
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let geoCoder = CLGeocoder()
-        var place = ""
-        currentLocation = locations.last
-        let currentLatitude = currentLocation.coordinate.latitude
-        let currentLongitude = currentLocation.coordinate.longitude
-        let currentCoodinates = "\(currentLatitude),\(currentLongitude)"
-        print(currentCoodinates)
-        dateLabel.text = currentCoodinates
-        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler: {
-            placemarks ,error in
-            if placemarks != nil{
-                let placemark = placemarks?.last
-                place = (placemark?.name)!
-            }
-            else{
-                print("error retrieving place")
-                place = "Unknown weather location"
-            }
-            self.locationsArray[0].name = place
-            self.locationsArray[0].coordinates = currentCoodinates
-            self.locationsArray[0].getWeather()
-            self.updateUserInterface()
-        })
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print ("failed to get user location")
-        
-    }
+    */
+
 }
--- WeatherGift/WeatherGift/PageVC.swift
+++ WeatherGift/WeatherGift/PageVC.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class PageVC: UIPageViewController {
 
+    
+    
     var currentPage = 0
-    var locationsArray = ["Local City", "Syndney", "Puerto Rico", "Accra"]
+    var locationsArray = [WeatherLocation]()
     var pageControl: UIPageControl!
     var listButton: UIButton!
     var barButtonWidth: CGFloat = 44
@@ -24,6 +26,10 @@ class PageVC: UIPageViewController {
         delegate=self
         dataSource=self
         
+        var newLocation = WeatherLocation(name: "", coordinates: "")
+        locationsArray.append(newLocation)
+        loadLocations()
+        
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
        
     }
@@ -35,17 +41,32 @@ class PageVC: UIPageViewController {
         
         
     }
+    func loadLocations(){
+        guard let locationsEncoded = UserDefaults.standard.value(forKey: "locationsArray") as? Data else{
+            print("could not load locationsArray from usersdefaults")
+            return
+        }
+        let decoder = JSONDecoder()
+        if let locationsArray = try? decoder.decode(Array.self, from: locationsEncoded) as [WeatherLocation]{
+            self.locationsArray = locationsArray
+        }
+        else{
+            print("ERROR: Couldnt decode data read form userdefaults")
+        }
+    }
     func configurePageControl(){
         let pageControlHeight: CGFloat = barButtonHeight
         let pageControlWidth: CGFloat = view.frame.width - (barButtonHeight * 2)
         let safeHeight = view.frame.height - view.safeAreaInsets.bottom
         
-        pageControl = UIPageControl(frame: CGRect(x: (view.frame.width - pageControlWidth)/2, y: safeHeight - pageControlHeight, width: pageControlWidth, height: pageControlHeight)
+        pageControl = UIPageControl(frame: CGRect(x: (view.frame.width - pageControlWidth)/2, y: safeHeight - pageControlHeight, width: pageControlWidth, height: pageControlHeight))
             
         pageControl.pageIndicatorTintColor=UIColor.lightGray
         pageControl.currentPageIndicatorTintColor = UIColor.blue
+        pageControl.backgroundColor = UIColor.white
         pageControl.numberOfPages = locationsArray.count
         pageControl.currentPage = currentPage
+        pageControl.addTarget(self, action: #selector(pageControlPressed), for: .touchUpInside)
         view.addSubview(pageControl)
     }
     
@@ -53,13 +74,29 @@ class PageVC: UIPageViewController {
         let safeHeight = view.frame.height - view.safeAreaInsets.bottom
         listButton = UIButton(frame: CGRect(x: view.frame.width - barButtonWidth, y: safeHeight - barButtonHeight, width: barButtonWidth, height: barButtonHeight))
         listButton.setImage(UIImage(named: "listbutton"), for: .normal)
-        listButton.setImage(UIImage(named: "listbutton-highlighted"), for: .highlighted)
-        listButton.addTarget(self, action: #selector(sequeToLocationVC), for: .touchUpInside)
+        listButton.setImage(UIImage(named: "listbutton-highlight"), for: .highlighted)
+        listButton.addTarget(self, action: #selector(sequeToListVC), for: .touchUpInside)
         view.addSubview(listButton)
     }
-    @objc func sequeToLocationVC(){
-        print("hey!youclicked me")
+    //segues
+    @objc func sequeToListVC(){
+        performSegue(withIdentifier: "ToListVC", sender: nil)
         
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let currentViewController = self.viewControllers?[0] as? DetailVC else {return}
+        locationsArray = currentViewController.locationsArray
+        if segue.identifier == "ToListVC"{
+            let destination = segue.destination as! ListVC
+            destination.locationsArray = locationsArray
+            destination.currentPage = currentPage
+        }
+    }
+    @IBAction func unwindFromListVC(sender: UIStoryboardSegue){
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        setViewControllers([createDetailVC(forPage: currentPage)], direction: .forward, animated: false, completion: nil)
     }
     func createDetailVC(forPage page: Int) -> DetailVC {
         
@@ -100,5 +137,13 @@ extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate{
             pageControl.currentPage = currentViewController.currentPage
         }
     }
-    
+    @objc func pageControlPressed(){
+        guard let currentViewController = self.viewControllers?[0] as? DetailVC else {return}
+        currentPage = currentViewController.currentPage
+        if pageControl.currentPage < currentPage{
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .reverse, animated: true, completion: nil)
+        }else if pageControl.currentPage > currentPage {
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .forward, animated: true, completion: nil)
+        }
+    }
 }
